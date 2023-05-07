7ddea2174bbf72e542f8ae80e882d5217a1075ae
NiccoloMorabito <morabito.niccolo@gmail.com>
2023-05-07 16:05:18 +0200

    Minor changes in data loaders

--- data_loaders.py
+++ data_loaders.py
@@ -3,40 +3,45 @@ from torch.utils.data import Dataset, DataLoader
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
 
-""" OLD METHOD TO LOAD DATA (FOR MULTIVARIATE REGRESSION)"""
+#TODO try to merge or standardize these two ways of loading data:
+# - associate better names to them
+# - scale in both
+# - both of them have to accept input features and output features to be parametrizable
+
+
+
 
 class MultivariateWakeDataset(Dataset):
-    def __init__(self, input_tensors_filepath, output_tensors_filepath):
-        self.input_tensors = torch.load(input_tensors_filepath)
-        self.output_tensors = torch.load(output_tensors_filepath)
-        
-        #TODO scale variables (here or somewhere else)
-        print(len(self.input_tensors), len(self.output_tensors))
-        assert len(self.input_tensors) == len(self.output_tensors)
+    def __init__(self, data_filepath: str, input_variables: list[str]): #TODO scaler????
+        #TODO MinMaxScaler or StandardScaler?
+        self._dataframe = pd.read_csv(data_filepath)
 
+        # Group by input features and create input and output tensors
+        inputs = []
+        outputs = []
+        for group, data in self._dataframe.groupby(input_variables):
+            inputs.append(torch.tensor(group, dtype=torch.float32))
+            outputs.append(torch.tensor(data['WS_eff'].values, dtype=torch.float32))
+
+        self.x = inputs
+        self.y = outputs
+        
     def __len__(self):
-        return len(self.input_tensors)
+        return len(self.x)
 
     def __getitem__(self, idx):
-        return self.input_tensors[idx], self.output_tensors[idx]
-    
-    def get_input_shape(self):
-        return self.input_tensors[0].shape[0]
+        return self.x[idx], self.y[idx]
 
-    def get_output_shape(self):
-        return self.output_tensors[0].shape[0]
-    
-def get_multivariate_dataloader(input_filepath: str, output_filepath: str, batch_size: int):
-    ds = MultivariateWakeDataset(input_filepath, output_filepath)
+def get_multivariate_dataloader(data_filepath: str, input_variables: list[str], batch_size: int):
+    ds = MultivariateWakeDataset(data_filepath, input_variables)
     return DataLoader(ds, batch_size=batch_size, shuffle=True)
 
 
-
-
 class UnivariateWakeDataset(Dataset):
-    def __init__(self, dataframe_csv: str, output_space: int, scaler = MinMaxScaler()):
+    def __init__(self, data_filepath: str, output_space: int, scaler = MinMaxScaler()):
         #TODO MinMaxScaler or StandardScaler?
-        self._dataframe = pd.read_csv(dataframe_csv)
+        self._dataframe = pd.read_csv(data_filepath)
+
         self.x = self._dataframe.iloc[:, :-output_space]
         self.x = torch.FloatTensor(scaler.fit_transform(self.x))
         self.y = self._dataframe.iloc[:, -output_space:]
@@ -49,6 +54,6 @@ class UnivariateWakeDataset(Dataset):
     def __getitem__(self, idx):
         return self.x[idx], self.y[idx]
 
-def get_univariate_dataloader(dataframe_csv: str, output_space: int, batch_size: int):
-    ds = UnivariateWakeDataset(dataframe_csv, output_space)
+def get_univariate_dataloader(data_filepath: str, output_space: int, batch_size: int):
+    ds = UnivariateWakeDataset(data_filepath, output_space)
     return DataLoader(ds, batch_size=batch_size, shuffle=True)
