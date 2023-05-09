0d8267c7978b7fe28fa46a895f1ca9c12f3edde0
NiccoloMorabito <morabito.niccolo@gmail.com>
2023-05-09 11:39:46 +0200

    Minor changes

--- data_loaders.py
+++ data_loaders.py
@@ -46,9 +46,16 @@ class WakeDataset(Dataset):
         # Group by input features and create input and output tensors
         inputs = list()
         outputs = list()
+
         for group, data in self.__df.groupby(self.__input_vars):
-            inputs.append(torch.FloatTensor(group))
-            outputs.append(torch.FloatTensor(data['WS_eff'].values))
+            input_tensor = torch.FloatTensor(group)
+            inputs.append(input_tensor)
+
+            #TODO so far only one output variable (WS_eff)
+            output_tensor = data.pivot(index='x', columns='y', values='WS_eff').values # 2d output tensor
+            #output_tensor = data['WS_eff'].values) #1d output tensor
+            output_tensor = torch.FloatTensor(output_tensor)
+            outputs.append(output_tensor)
 
         self.x = torch.stack(inputs, dim=0)
         self.x = torch.FloatTensor(self.__scaler.fit_transform(self.x))
@@ -64,4 +71,4 @@ def get_wake_dataloader(data_filepath: str, input_variables: list[str], output_v
                         scaler: MinMaxScaler(), #TODO
                         batch_size: int):
     ds = WakeDataset(data_filepath, input_variables, output_variables, scaler)
-    return DataLoader(ds, batch_size=batch_size, shuffle=True)
+    return DataLoader(ds, batch_size=batch_size) #, shuffle=True)
