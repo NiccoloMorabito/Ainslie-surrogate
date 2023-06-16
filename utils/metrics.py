52a21d1d1536b11265ad0bc50a8841c2aa0966e4
NiccoloMorabito <morabito.niccolo@gmail.com>
2023-06-16 19:01:06 +0200

    Added code to compute overall training time + minor improvements

--- utils/metrics.py
+++ utils/metrics.py
@@ -51,10 +51,13 @@ class EpochTimer:
 
 EPOCH_TIME_LABEL = "epoch_time (seconds)"
 
+#TODO change the code considering that epoch_num starts at 0, so it would be more readable adding 1
 class MetricsLogger:
 
-    def __init__(self, name: str, df_metrics: pd.DataFrame | None = None) -> None:
+    def __init__(self, name: str, automatic_save_after: int = 50,
+                 df_metrics: pd.DataFrame | None = None) -> None:
         self.name = name
+        self.automatic_save_after = automatic_save_after
         if df_metrics is None:
             self.__epoch_to_metrics = dict()
             self.__logged_metrics = set()
@@ -65,14 +68,17 @@ class MetricsLogger:
             for index, row in df_metrics.iterrows():
                 self.__epoch_to_metrics[index] = row.to_dict()
             self.__logged_metrics = set(self.__epoch_to_metrics[0].keys())
+            #TODO self.__logged_metrics.remove(EPOCH_TIME_LABEL)
             self.df = df_metrics
             self.__logging = False
+            print(f"Logged the following metrics for {name}: {self.get_logged_metric_names()}")
         self.__epoch_timer = None
         self.timestamp = datetime.datetime.now().strftime("%d-%m-%Y_%H-%M-%S")
     
     def log_metric(self, epoch_num: int, metric_name: str, metric_value: float) -> None:
         # intermediate savings (TODO the logging boolean in this case doesn't make sense)
-        if epoch_num not in self.__epoch_to_metrics.keys() and epoch_num%50==0 and epoch_num>0:
+        if epoch_num not in self.__epoch_to_metrics.keys() and \
+            epoch_num%self.automatic_save_after==0 and epoch_num>0:
             self.__save_intermediate_metrics()
         self.__logging = True
         if epoch_num not in self.__epoch_to_metrics.keys():            
@@ -187,8 +193,42 @@ class MetricsLogger:
         self.df.to_csv(filepath)
         print(f"Metrics exported in the following csv file: {filepath}")
     
+    def get_training_time(self, according_to: str = 'Validation loss') -> int:
+        """Overall training time of the best model"""
+        if according_to not in self.__logged_metrics:
+            raise ValueError(f"The metric {according_to} has not been logged")
+
+        if self.__logging:
+            self.__stop()
+        
+        final_epoch = self.__find_best_model_epoch(according_to)
+        training_time = self.__sum_training_times(final_epoch)
+        print(f"The best model has been generated during the {final_epoch}th epoch"+\
+              f" and it took {training_time} seconds for training.")
+        return training_time
+        
+    def __find_best_model_epoch(self, according_to: str) -> int:
+        # considering that 'according_to' is a loss -> the smallest the better
+        #TODO add code to consider also other metrics (e.g. accuracy) with opposite behaviour
+        final_epoch = 0
+        best_value = 1_000_000.
+        for epoch, metrics in self.__epoch_to_metrics.items():
+            value = metrics[according_to]
+            if value < best_value:
+                best_value = value
+                final_epoch = epoch
+        return final_epoch
+
+    def __sum_training_times(self, final_epoch: int) -> int:
+        training_time = 0
+        for epoch, metrics in self.__epoch_to_metrics.items():
+            if epoch >= final_epoch:
+                break
+            training_time += metrics[EPOCH_TIME_LABEL]
+        return int(training_time)
+    
     @staticmethod
     def from_csv(filepath: str):
         logs_df = pd.read_csv(filepath, header=0, index_col='epoch #')
-        metrics_logger = MetricsLogger(filepath.split("/")[1].split("_")[0], logs_df)
+        metrics_logger = MetricsLogger(name=filepath.split("/")[1].split("_")[0], df_metrics=logs_df)
         return metrics_logger
